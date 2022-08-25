b975cca9c30ea3d96aaa7bc4298e129ff1ccb25b
saihnii4 <81787757+saihnii4@users.noreply.github.com>
2022-08-25 20:29:01 +0800

    no way bruh
    no way bruh

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -6,10 +6,21 @@ capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 
 local config = {
   sumneko_lua = {
-    on_attach = function()
-      print("Hello World")
-      map.n.nore.silent['<Leader>h'] = function() (require'notify')('TODO: load Haskell REPL') end -- TODO: load Haskell REPL
-    end,
+    settings = {
+      Lua = {
+        runtime = {
+          version = "Lua 5.4.4",
+          path = {
+            '?.lua',
+            '?/init.lua',
+            vim.fn.expand '~/.luarocks/share/lua/5.4/?.lua',
+            vim.fn.expand '~/.luarocks/share/lua/5.4/?/init.lua',
+            '/usr/share/5.4/?.lua',
+            '/usr/share/lua/5.4/?/init.lua'
+          }
+        }
+      }
+    },
     --cmd = { vim.g.HOME .. ".local/bin/lua-language-server", "-E", vim.g.HOME .. ".local/lib/lua-language-server" },
     cmd = { "/usr/bin/lua-language-server", "-E", "/usr/lib/lua-language-server" },
     diagnostics = {
@@ -34,7 +45,7 @@ local config = {
   hls = {
     on_attach = function()
       print("Hello World")
-      map.n.nore.silent['<Leader>h'] = function() (require'notify')('TODO: load Haskell REPL') end -- TODO: load Haskell REPL
+      map.n.nore.silent['<Leader>h'] = function() (require 'notify')('TODO: load Haskell REPL') end -- TODO: load Haskell REPL
     end
   },
   ccls = true,
@@ -46,7 +57,10 @@ for k, v in pairs(config) do
 
   if type(v) ~= "boolean" then
     lsp[k].setup(v)
+    goto continue
   end
 
   lsp[k].setup {}
+
+  ::continue::
 end
